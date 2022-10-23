2b16a8f4ea45c419eb3ee00a2fdaf089c53b2c72
Wojtek Erbetowski <wojtek@erbetowski.pl>
2022-10-23 16:14:21 +0200

    Prepare for task 5

---
+++ tasks/5-storage.md
@@ -0,0 +1,11 @@
+* Traverse directories
+  * Rebuild commands to support directories instead fo files
+  * Store the output to JSON, sqlite* or print it out
+  * Let the tests navigate you
+
+Extra tasks:
+* Add missing tests
+  * End to end (calculate and verify)
+  * Parametrize tests
+  * Different algorithms
+* Add SQLite support
--- tests/test_hashing.py
+++ tests/test_hashing.py
@@ -1,16 +1,15 @@
-import subprocess
+import json
 from pathlib import Path
 
 import pytest
 from click.testing import CliRunner
 
 import my_script
-from my_script import main
 
 
 @pytest.fixture
 def hello_file(tmpdir):
-    test_file = Path(tmpdir, "filename")
+    test_file = Path(tmpdir, "a")
 
     with(test_file.open("w")) as f:
         f.write("Hello")
@@ -25,50 +24,130 @@ def runner():
 
 class TestHashVerification:
 
-    def test_fails_on_missing_file(self, runner):
-        p = runner.invoke(my_script.verify, ["missing_file"])
-
-        assert p.exit_code
-
-    def test_successful_verification(self, hello_file: Path, runner):
-        expected_sum = "f7ff9e8b7bb2e09b70935a5d785e0cc5d9d0abf0"
-
+    def test_empty_directory_stdout(self, runner, tmpdir):
         command = runner.invoke(
-            my_script.main,
+            my_script.calculate,
             [
-                "verify",
-                str(hello_file),
-                expected_sum,
+                str(tmpdir),
             ],
         )
 
         assert command.exit_code == 0
 
-    def test_verification_failure(self, hello_file: Path, runner):
-        expected_sum = "wrong sum"
+        assert command.stdout == ""
+
+    def test_empty_directory_json(self, runner, tmpdir):
+        storage = Path(tmpdir, "storage.json")
+
+        command = runner.invoke(
+            my_script.calculate,
+            [
+                str(tmpdir),
+                "--storage",
+                str(storage),
+            ],
+        )
+
+        assert command.exit_code == 0
+
+        assert command.stdout == ""
+
+        with storage.open() as f:
+            out = json.load(f)
+
+        assert out == {}
+
+    def test_successful_verification_stdin(self, tmpdir, runner):
+        file_path = "filename"
+        file_content = "Hello"
+        expected_sum = "f7ff9e8b7bb2e09b70935a5d785e0cc5d9d0abf0"
+
+        with Path(tmpdir, file_path).open("w") as f:
+            f.write(file_content)
+
 
         command = runner.invoke(
             my_script.main,
             [
                 "verify",
-                str(hello_file),
-                expected_sum,
+                str(tmpdir),
             ],
+            input=f"{file_path}:{expected_sum}",
+        )
+
+        assert command.exit_code == 0
+
+    def test_successful_verification_json(self, tmpdir, runner):
+        hash_dir = Path(tmpdir, "dir")
+        hash_dir.mkdir()
+
+        file_path = "filename"
+        file_content = "Hello"
+        expected_sum = "f7ff9e8b7bb2e09b70935a5d785e0cc5d9d0abf0"
+
+        with Path(hash_dir, file_path).open("w") as f:
+            f.write(file_content)
+
+        with Path(tmpdir, "storage.json").open("w") as f:
+            json.dump({
+                file_path: expected_sum,
+            }, f)
+
+        command = runner.invoke(
+            my_script.main,
+            [
+                "verify",
+                str(Path(tmpdir, "dir")),
+                "--storage",
+                str(Path(tmpdir, "storage.json")),
+            ],
+        )
+
+        assert command.exit_code == 0
+
+    def test_failed_verification_stdin(self, tmpdir, runner):
+        file_path = "filename"
+        file_content = "Hello"
+        expected_sum = "wrong hash"
+
+        with Path(tmpdir, file_path).open("w") as f:
+            f.write(file_content)
+
+
+        command = runner.invoke(
+            my_script.main,
+            [
+                "verify",
+                str(tmpdir),
+            ],
+            input=f"{file_path}:{expected_sum}",
         )
 
         assert command.exit_code != 0
 
-    def test_verification_wrong_algo(self, hello_file: Path, runner):
-        expected_sum = "f7ff9e8b7bb2e09b70935a5d785e0cc5d9d0abf0"
+    def test_failed_verification_json(self, tmpdir, runner):
+        hash_dir = Path(tmpdir, "dir")
+        hash_dir.mkdir()
+
+        file_path = "filename"
+        file_content = "Hello"
+        expected_sum = "wrong hash"
+
+        with Path(hash_dir, file_path).open("w") as f:
+            f.write(file_content)
+
+        with Path(tmpdir, "storage.json").open("w") as f:
+            json.dump({
+                file_path: expected_sum,
+            }, f)
 
         command = runner.invoke(
             my_script.main,
             [
                 "verify",
-                str(hello_file),
-                expected_sum,
-                "--algorithm",
-                "md5",
+                str(Path(tmpdir, "dir")),
+                "--storage",
+                str(Path(tmpdir, "storage.json")),
             ],
         )
 
