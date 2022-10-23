d54860e6be62e0143a446ad0cdac77a3ee11c083
Wojtek Erbetowski <wojtek@erbetowski.pl>
2022-10-23 14:00:28 +0200

    Prepare for task 3

--- my_script.py
+++ my_script.py
@@ -1,4 +1,18 @@
-import black
+import os
+import sys
+from hashlib import sha1
 
 
-print(f"Black version: {black.__version__}")
+def main():
+    filename = sys.argv[1]
+
+    with open(filename, "rb") as f:
+        file_content = f.read()
+
+    hashsum = sha1(file_content).hexdigest()
+
+    print(hashsum, end="")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tasks/3-testing.md
@@ -0,0 +1,5 @@
+* Improve the hashing test
+  * Add more test cases (e.g. empty file)
+  * Create the temporary working directory
+  * Prepare the test files (write the proper content)
+  * Use `pytest` fixture to simplify the test
